0249860c23f11d005ff252d091596177a56e8106
vapapa <infovapapa@gmail.com>
2024-12-23 21:29:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Datos simulados de tracking
 const trackingData = {
   "YWMIA010005902385": "arrived",
-  "papa": "arrived",  // Ya llegó
+  "JPG": "arrived",  // Ya llegó
   "456DEF": "not-arrived", // No ha llegado
   "789GHI": "arrived",  // Ya llegó
 };
